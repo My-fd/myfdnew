462ed9d8749334d6d8dc446a6f8c04cfec726d14
Egor Zobkov <zewine@yandex.ru>
2023-08-21 19:37:43 +0300

    начал обновлять хидер

--- resources/views/web/base.blade.php
+++ resources/views/web/base.blade.php
@@ -2,47 +2,57 @@
 <html>
 <head>
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="{{ route('web.home') }}">My-Fd</a>
+
+<header class="header">
+    <div class="header__nav" id="navbarNav">
+        <a href="{{ route('web.home') }}" class="header-logo">
+            <img class="header-logo-img" src="./img/logo.svg">
+        </a>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+
+        <nav>   
+            <ul class="list-reset navbar-nav">
                 <li class="nav-item active">
                     <a class="nav-link" href="{{ route('web.home') }}">{{ __('Home') }}</a>
                 </li>
                 @auth
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('web.listings.createAd') }}">{{ __('Create Ad') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('web.messages.index') }}">{{ __('Messages') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('web.user.account') }}">{{ __('Profile') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('web.logout') }}">{{ __('Logout') }}</a>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('web.listings.createAd') }}">{{ __('Create Ad') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('web.messages.index') }}">{{ __('Messages') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('web.user.account') }}">{{ __('Profile') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('web.logout') }}">{{ __('Logout') }}</a>
+                </li>
                 @endauth
                 @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('web.register') }}">{{ __('Register') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('web.login') }}">{{ __('Login') }}</a>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('web.register') }}">{{ __('Register') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('web.login') }}">{{ __('Login') }}</a>
+                </li>
                 @endguest
             </ul>
-        </div>
+        </nav>
     </div>
-</nav>
+</header>
+
+
+
 
 <div class="container mt-4">
     @yield('content')
@@ -51,7 +61,7 @@
 <!-- Footer -->
 <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
-        <span class="text-muted">{{ __('Place sticky footer content here.') }}</span>
+        <span class="text-muted">{{ __('Place stickygfdgdffgdffdfdg footer content here.') }}</span>
     </div>
 </footer>
 
