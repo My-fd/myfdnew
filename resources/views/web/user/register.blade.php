c62ac59216d2ab5e71ffd045d64a7b0ebaf3eb77
Egor Zobkov <zewine@yandex.ru>
2023-09-26 20:12:51 +0300

    Поправил страницу регистрации

--- resources/views/web/user/register.blade.php
+++ resources/views/web/user/register.blade.php
@@ -12,10 +12,10 @@
                             @csrf
 
                             <div class="row mb-3">
-                                <label for="nickname" class="col-md-4 col-form-label text-md-end">{{ __('Nickname') }}</label>
+                                <!-- <label for="nickname" class="col-md-4 col-form-label text-md-end">{{ __('Nickname') }}</label> -->
 
-                                <div class="col-md-6">
-                                    <input id="nickname" type="text" class="form-control @error('nickname') is-invalid @enderror" name="nickname" value="{{ old('nickname') }}" required autocomplete="nickname">
+                                <div class="form-group">
+                                    <input id="nickname" type="text" class="form-control @error('nickname') is-invalid @enderror" name="nickname" value="{{ old('nickname') }}" required autocomplete="nickname" placeholder="{{ __('Nickname') }}">
 
                                     @error('nickname')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +26,10 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
+                                <!-- <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label> -->
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <div class="form-group">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="{{ __('Email') }}">
 
                                     @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -40,10 +40,10 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                <!-- <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label> -->
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <div class="form-group">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="{{ __('Password') }}">
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -54,10 +54,10 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                                <!-- <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label> -->
 
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <div class="form-group">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="{{ __('Confirm Password') }}">
                                 </div>
                             </div>
 
